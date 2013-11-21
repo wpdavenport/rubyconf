0801406798a176f22b10befe350cbf1e4a67edfb
wpdavenport <wpdavenport@gmail.com>
2013-11-20 21:56:23 -0600

    Knowingly adding ability to allow outside apps to use my API.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+   before_filter :cors_set_access_control_headers
+   
+   private
+   
+   def cors_set_access_control_headers
+     headers['Access-Control-Allow-Origin'] = '*'
+     headers['Access-Control-Allow-Methods'] = 'PUT, POST, GET, OPTIONS'
+     headers['Access-Control-Allow-Headers'] = 'Content-Type, X-Requested-With, X-Prototype-Version'
+   end
 end
